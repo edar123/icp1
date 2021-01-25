02dbcc7bbc77d92d8d26f45ea28e2b16d17f0d7a
edar123 <63585043+edar123@users.noreply.github.com>
2021-01-24 18:41:42 -0600

    Add files via upload

---
+++ 2nd1icp1.py
@@ -0,0 +1,16 @@
+##2.(ii)Take two numbers from user and perform arithmetic operations on them.
+
+firstNumber = input('Please enter an integer: ')#####inputting 1st number from user
+secondNumber = input('Please enter a second integer: ')#####inputting 2nd number from user
+
+num1 = int(firstNumber)###converting values to integer
+num2 = int(secondNumber)###converting values to integer
+
+
+
+print('Here is some output')###printing output
+
+print(num1, ' + ', num2, ' = ', num1+num2)####addition operation
+print(num1,' - ',num2, ' = ',num1-num2)####subtraction operation
+print(num1,' * ',num2, ' = ',num1*num2)####multiplication operation
+print(num1,' / ',num2, ' = ',num1/num2)####division operation
---
+++ 3rd icp1.py
@@ -0,0 +1,4 @@
+##3.  Write a program that accepts a sentence and replace each occurrence of ‘python’ with ‘pythons’ without using regex
+inString = input("input a sentence:")###reading sentence from user
+outString = inString.replace('Python', 'Pythons')###using replace function to search and replace string
+print(outString)##printing Output
